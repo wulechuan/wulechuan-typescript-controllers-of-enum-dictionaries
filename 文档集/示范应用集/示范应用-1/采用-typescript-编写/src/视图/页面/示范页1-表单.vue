2b7692988dd26ba07559b1f467219c5fc4fc5af1
吴乐川 <wulechuan@live.com>
2022-07-29 20:05:57 +0800

    主体已成功！但特别条目之【呈示称谓】不能凭【凭某条目之值求其呈示称谓】取得。

--- 文档集/示范应用集/示范应用-1/采用-typescript-编写/src/视图/页面/示范页1-表单.vue
+++ 文档集/示范应用集/示范应用-1/采用-typescript-编写/src/视图/页面/示范页1-表单.vue
@@ -8,7 +8,7 @@
                             v-model="表单之数据['填写项1']"
                             placeholder="请选择一种中华名典"
                             clearable
-                            @change="当某表单项取值变动后('填写项（一）', $event, 字典机_字典甲)"
+                            @change="当某表单项之值变动后('填写项（一）', $event, 字典机_字典甲)"
                         >
                             <template v-for="字典界面项 in 字典界面项之列表_字典甲" >
                                 <el-option
@@ -25,7 +25,7 @@
                             v-model="表单之数据['填写项2']"
                             placeholder="请选择一种中华服饰"
                             clearable
-                            @change="当某表单项取值变动后('填写项（二）', $event, 字典机_字典乙)"
+                            @change="当某表单项之值变动后('填写项（二）', $event, 字典机_字典乙)"
                         >
                             <template v-for="字典界面项 in 字典界面项之列表_字典乙" >
                                 <el-option
@@ -42,7 +42,7 @@
                             v-model="表单之数据['填写项3']"
                             placeholder="请选择一种中华明代服饰"
                             clearable
-                            @change="当某表单项取值变动后('填写项（三）', $event, 字典机_字典丙)"
+                            @change="当某表单项之值变动后('填写项（三）', $event, 字典机_字典丙)"
                         >
                             <template v-for="字典界面项 in 字典界面项之列表_字典丙" >
                                 <el-option
@@ -96,7 +96,7 @@ import{
 
 
 @Component({})
-export default class Page示范页1_常见的双列互通之形式 extends Vue {
+export default class Page示范页1 extends Vue {
     private 表单之数据 = {
         填写项1: '',
         填写项2: '',
@@ -109,9 +109,9 @@ export default class Page示范页1_常见的双列互通之形式 extends Vue {
     private get 字典机_字典乙 (): null | 范_本应用中一切字典机 { return this.字典总机 && this.字典总机.为.取某字典机('字典乙') }
     private get 字典机_字典丙 (): null | 范_本应用中一切字典机 { return this.字典总机 && this.字典总机.为.取某字典机('字典丙') }
 
-    private get 字典界面项之列表_字典甲 (): 范_本应用中一切字典之字典条目之视式[] { return !this.字典总机 ? [] : this.字典总机.为.取某字典之字典条目之视式之列表('字典甲') }
-    private get 字典界面项之列表_字典乙 (): 范_本应用中一切字典之字典条目之视式[] { return !this.字典总机 ? [] : this.字典总机.为.取某字典之字典条目之视式之列表('字典乙') }
-    private get 字典界面项之列表_字典丙 (): 范_本应用中一切字典之字典条目之视式[] { return !this.字典总机 ? [] : this.字典总机.为.取某字典之字典条目之视式之列表('字典丙') }
+    private get 字典界面项之列表_字典甲 (): 范_本应用中一切字典之字典条目之视式[] { return !this.字典总机 ? [] : this.字典总机.为.取某字典诸条目之视式_其列表('字典甲') }
+    private get 字典界面项之列表_字典乙 (): 范_本应用中一切字典之字典条目之视式[] { return !this.字典总机 ? [] : this.字典总机.为.取某字典诸条目之视式_其列表('字典乙') }
+    private get 字典界面项之列表_字典丙 (): 范_本应用中一切字典之字典条目之视式[] { return !this.字典总机 ? [] : this.字典总机.为.取某字典诸条目之视式_其列表('字典丙') }
 
 
 
@@ -134,13 +134,15 @@ export default class Page示范页1_常见的双列互通之形式 extends Vue {
                     该字典之唯一标识: '字典丙',
 
                     配置杂集: {
-                        该字典并不参与字典群总机发起的所谓数据自动构建任务: true,
+                        该字典在默认情形下不参与字典群总机发起的数据重建任务: true,
+                    },
 
-                        代表全选的字典条目: {
+                    数据集: {
+                        字典特别条目_其代表全选者: {
                             字典条目之视式: {
                                 唯一标识: '-全-',
                                 值: '-全-',
-                                // 呈示称谓: '〔全选〕',
+                                呈示称谓: '〔我包圆了〕',
                             },
                         },
                     },
@@ -150,11 +152,11 @@ export default class Page示范页1_常见的双列互通之形式 extends Vue {
                             const { 字典机_字典乙 } = this
                             if (!字典机_字典乙) { return Promise.resolve([]) }
 
-                            const 字典乙之字典条目之原式集 = 字典机_字典乙.数据集.字典条目之原式集
-                            if (!Array.isArray(字典乙之字典条目之原式集)) { return Promise.resolve([]) }
+                            const 字典乙之字典诸条目之原式_其列表 = 字典机_字典乙.数据集.字典诸条目之原式_其列表
+                            if (!Array.isArray(字典乙之字典诸条目之原式_其列表)) { return Promise.resolve([]) }
 
                             return Promise.resolve(
-                                字典乙之字典条目之原式集.filter(乙之某字典条目之原式 => /明制/.test(乙之某字典条目之原式.label))
+                                字典乙之字典诸条目之原式_其列表.filter(乙之某字典条目之原式 => /明制/.test(乙之某字典条目之原式.label))
                             )
                         },
                     },
@@ -185,8 +187,8 @@ export default class Page示范页1_常见的双列互通之形式 extends Vue {
         document.title = '字典群之总机·例1：表单（TypeScript）'
     }
 
-    private 当某表单项取值变动后 (所涉填写项之称谓: string, 新值: string, 所涉字典机: 范_本应用中一切字典机) {
-        const 新值之称谓 = 所涉字典机.为.求某项值之称谓(新值)
+    private 当某表单项之值变动后 (所涉填写项之称谓: string, 新值: string, 所涉字典机: 范_本应用中一切字典机) {
+        const 新值之称谓 = 所涉字典机.为.凭某条目之值求其呈示称谓(新值)
         console.log(`填写项【${所涉填写项之称谓}】的取值变动了。变为：`, 新值之称谓, `（${新值}）。`)
     }
 }
