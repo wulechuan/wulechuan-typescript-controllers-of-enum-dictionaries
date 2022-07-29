8537f54fdb712bf8258a8ed3585645531331c2cd
吴乐川 <wulechuan@live.com>
2022-07-29 16:33:19 +0800

    辅助工具之改进。

--- 源代码/原始的源代码/typescript/辅助工具集_通用.ts
+++ 源代码/原始的源代码/typescript/辅助工具集_通用.ts
@@ -4,15 +4,23 @@ export type 范_任意对象 = { [键: string]: any; };
 
 
 
-export function 是合规的非列表对象 (待检验的值: any): 待检验的值 is 范_任意对象 {
-    return !!待检验的值 && typeof 待检验的值 === 'object' && !Array.isArray(待检验的值)
+export function 是合规的非列表对象 <
+    范_期望之形态 extends 范_任意对象 = 范_任意对象
+>(
+    待验值: unknown
+): 待验值 is 范_期望之形态 {
+    return !!待验值 && typeof 待验值 === 'object' && !Array.isArray(待验值)
 }
 
 
 
-export function 求合规的非列表对象_若不合规则给出null <范_期望之形态 extends 范_任意对象 = 范_任意对象>(待检验的值: any): null | 范_期望之形态 {
-    if (!!待检验的值 && typeof 待检验的值 === 'object' && !Array.isArray(待检验的值)) {
-        const 合规的值: 范_期望之形态 = 待检验的值
+export function 求合规的非列表对象_若不合规则给出null <
+    范_期望之形态 extends 范_任意对象 = 范_任意对象
+>(
+    待验值: unknown
+): null | 范_期望之形态 {
+    if (!!待验值 && typeof 待验值 === 'object' && !Array.isArray(待验值)) {
+        const 合规的值 = 待验值 as 范_期望之形态
         return 合规的值
     } else {
         return null
@@ -21,9 +29,13 @@ export function 求合规的非列表对象_若不合规则给出null <范_期�
 
 
 
-export function 求合规的非列表对象_按需构造空的对象 <范_期望之形态 extends 范_任意对象 = 范_任意对象>(待检验的值: any): 范_期望之形态 {
-    if (!!待检验的值 && typeof 待检验的值 === 'object' && !Array.isArray(待检验的值)) {
-        const 合规的值: 范_期望之形态 = 待检验的值
+export function 求合规的非列表对象_若不合规则给出空的对象 <
+    范_期望之形态 extends 范_任意对象 = 范_任意对象
+>(
+    待验值: unknown
+): 范_期望之形态 {
+    if (!!待验值 && typeof 待验值 === 'object' && !Array.isArray(待验值)) {
+        const 合规的值 = 待验值 as 范_期望之形态
         return 合规的值
     } else {
         return {} as 范_期望之形态
